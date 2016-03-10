31768eb4bd16c458f37196b3baed00c662949783
Marcus Cooke <marcus.j.cooke@gmail.com>
2016-03-10 10:21:58 +0000

    expanded mobile menu buttons.

--- template-parts/mobile-top-bar.php
+++ template-parts/mobile-top-bar.php
@@ -47,7 +47,7 @@
 	</div>
 	<div class="menu-centered overlay" id="mobile-menu" role="navigation" data-toggler data-animate="slide-in-down slide-out-up" style="display: none;">
   	<?php foundationpress_mobile_nav(); ?> 
-  	<a href="tel:01303764287" class="button">Call: 01303 764287</a>
-  	<a href="/contact" class="secondary button">Request a call back</a>
+  	<a href="tel:01303764287" class="button expanded">Call: 01303 764287</a>
+  	<a href="/contact" class="secondary button expanded">Request a call back</a>
 	</div>
 </div>
