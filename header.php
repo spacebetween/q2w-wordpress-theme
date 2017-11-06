3db6c8f772460728043f42d0091a5e61349ffbae
Luke Frake <lukefrake@gmail.com>
2017-11-06 17:25:48 +0000

    Update GTM

--- header.php
+++ header.php
@@ -25,13 +25,13 @@
 	</head>
 	<body <?php body_class(); ?>>
 	<!-- Google Tag Manager -->
-	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KDKL7V"
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MDNWMD6"
 	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-KDKL7V');</script>
+	})(window,document,'script','dataLayer','GTM-MDNWMD6');</script>
 	<!-- End Google Tag Manager -->
 	<?php do_action( 'foundationpress_after_body' ); ?>
 
